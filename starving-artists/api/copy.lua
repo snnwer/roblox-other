b45be93f74c0ec0756c26ba6ad76eeeaa5cb642c
snnwer <75314624+snnwer@users.noreply.github.com>
2022-04-18 12:17:54 +0500

    copy

---
+++ starving-artists/api/copy.lua
@@ -0,0 +1,26 @@
+local player = game.Players.LocalPlayer
+
+local lib = {}
+
+function lib:Copy(victim, number: number)
+	victim = victim or return false
+	number = number or return false
+	local tg = workspace.Plots[victim.Name].Easels[number].Canvas.SurfaceGui.Grid
+
+	function copyGrid()
+		local destination = player.PlayerGui.MainGui.PaintFrame.GridHolder.Grid
+
+		for i = 1, 1024 do
+			destination[i].BackgroundColor3 = tg[i].BackgroundColor3
+			if #tg[i]:GetChildren() > 0 then
+				for a,b in next, tg[i]:GetChildren() do
+					b:Clone().Parent = destination[i]
+				end
+			end
+		end
+	end
+
+	copyGrid()
+
+end
+return lib
