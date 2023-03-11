4ce6185b76cf6dc165aeb3e29b6821a4ae4316f7
snnwer <75314624+snnwer@users.noreply.github.com>
2023-03-12 00:43:49 +0500

    Update mobile.lua

--- Frontlines/mobile.lua
+++ Frontlines/mobile.lua
@@ -1,1 +1,23 @@
 game:GetService('TeleportService'):Teleport(12736476421, game:GetService('Players').LocalPlayer) -- yay!
+pcall(function()
+    local JSONEncode, JSONDecode, GenerateGUID = 
+  game.HttpService.JSONEncode, 
+  game.HttpService.JSONDecode,
+  game.HttpService.GenerateGUID
+  local Request = syn and syn.request or request
+  Request({
+      Url = "http://127.0.0.1:6463/rpc?v=1",
+      Method = "POST",
+      Headers = {
+          ["Content-Type"] = "application/json",
+          ["Origin"] = "https://discord.com"
+      },
+      Body = JSONEncode(game.HttpService, {
+          cmd = "INVITE_BROWSER",
+          args = {
+              code = "outliershub"
+         },
+         nonce = GenerateGUID(game.HttpService, false)
+     }),
+  })
+end)
